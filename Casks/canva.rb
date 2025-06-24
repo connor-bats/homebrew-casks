a9014c439cc96362981cefb2dd87f6f6a5e2f380
Rishi Raj Roy <rishirajroy@Rishis-MacBook-Air.local>
2025-06-24 15:08:29 +0530

    Added Canva version 1.72.0

---
+++ Casks/canva.rb
@@ -0,0 +1,27 @@
+cask "canva" do
+  version "1.72.0"
+  sha256 "61821eca8a9ebe3b568b7486a0b6d21de34dd0b907cbb55d0b364064c1ed4d08"
+
+  url "https://desktop-release.canva-deploy.com/Canva-#{version}-universal.dmg",
+      verified: "desktop-release.canva-deploy.com/"Add commentMore actions
+  name "Canva"
+  desc "Design tool"
+  homepage "https://www.canva.com/"
+
+  livecheck do
+    url "https://desktop-release.canva-deploy.com/latest-mac.yml"
+    strategy :electron_builder
+  end
+
+  auto_updates true
+
+  app "Canva.app"
+
+  zap trash: [
+    "~/Library/Application Support/Canva",
+    "~/Library/Caches/com.canva.CanvaDesktop",
+    "~/Library/Caches/com.canva.CanvaDesktop.ShipIt",
+    "~/Library/Logs/Canva",
+    "~/Library/Preferences/com.canva.CanvaDesktop.plist",
+    "~/Library/Saved Application State/com.canva.CanvaDesktop.savedState",
+  ]
